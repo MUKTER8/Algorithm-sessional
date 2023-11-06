e8db6bff7fccfb578c319ad8436cbe8bf4716237
MUKTER HOSAIN <mokterprodhan8@gmail.com>
2023-11-06 18:48:20 +0600

    changes

--- 0/0/knapsack dynamic programming.c
+++ 0/0/knapsack dynamic programming.c
@@ -11,4 +11,25 @@ int main(){
     for(i=0;i<n;i++){
         scanf("%d",&wts[i]);
     }
+    int dp[n+1][n+1];
+    for(i=0;i<=n;i++){
+        for(j=0;j<=n;j++){
+            dp[i][j]=dp[i-1][j];
+            if(j>=wts[i-1]){
+                int rcap =j-wts[i-1];
+                if(dp[i-1][rcap]+val[i-1]>dp[i-1][j]){
+                    dp[i][j]=dp[i-1][rcap]+val[i-1];
+                }
+                else {
+                    dp[i][j]= dp[i-1][j];
+                }
+
+            }
+            else{
+                dp[i][j]=dp[i-1][j];
+            }
+        }
+        printf("%d\n",dp[n][cap]);
+    }
+    return 0;
 }
